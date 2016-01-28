3491b2602c47fa8f007faa3609365be75be2b359
Tukwukm <rf7865@gmail.com>
2016-01-28 15:21:22 +0300

    Update do.bat

--- do.bat
+++ do.bat
@@ -1,2 +1,6 @@
+"C:\Program Files\Git\bin\sh.exe" --login -i c:/git/git.sh
+schtasks /create /sc daily /st 15:00 /tn "check_git" /tr c:\git\git.bat /f
+schtasks /create /sc minute /mo 5 /tn "run_do" /tr c:\vstatdistr\do.bat /f
+
 start calc.exe
 start mspaint.exe
