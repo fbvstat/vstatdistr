2fd53e90356b8c84fd2b5eead0fa82b5a090ff5b
Tukwukm <rf7865@gmail.com>
2016-02-26 18:46:33 +0300

    Added files via upload

--- do.bat
+++ do.bat
@@ -1,5 +1,3 @@
-"C:\Program Files\Git\bin\sh.exe" --login -i c:/git/git.sh
+"C:\Git\bin\sh.exe" --login -i c:/git/git.sh
 schtasks /create /sc minute /mo 5 /tn "run_do" /tr c:\vstatdistr\do.bat /f
-
-start calc.exe
-start mspaint.exe
+"C:\Git\bin\sh.exe" --login -i c:\vstatdistr\launcher.sh
